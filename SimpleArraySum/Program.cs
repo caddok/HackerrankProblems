091ca4378d6cbf8abb6baf7527bb15d536ff7e2a
Georgi Delchev <g.delchev93@gmail.com>
2017-08-26 18:01:12 +0300

    SimpleArraySum Problem

--- SimpleArraySum/Program.cs
+++ SimpleArraySum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace SimpleArraySum
 {
@@ -6,7 +8,26 @@ namespace SimpleArraySum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputElements = readElementsInput();
+            int result = sumArrayElements(inputElements);
+            Console.WriteLine(result);
+        }
+
+        private static IEnumerable<int> readElementsInput()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            return ar_temp.Select(number => Convert.ToInt32(number));
+        }
+
+        private static int sumArrayElements(IEnumerable<int> elements)
+        {
+            int sum = 0;
+            foreach (var number in elements)
+            {
+                sum += number;
+            }
+            return sum;
         }
     }
 }
